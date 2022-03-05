21f3c3b96699eec62834ac17f74f4190ec193e8c
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-03-05 20:14:46 -0300

    exibir valores na tela

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output,EventEmitter } from '@angular/core';
+
 
 
 
@@ -9,12 +10,20 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent{
 
+    @Output() aoTransferir = new EventEmitter<any>();
+
     valor: number;
     destino: number;
 
     transferir(){
         console.log('Solicitada nova tranferencia');
-        console.log('Valor:' , this.valor);
-        console.log('Destino', this.destino);
+        const valorEmitir = {valor: this.valor,destino: this.destino};
+        this.aoTransferir.emit(valorEmitir);
+        this.limparCampos();
+    }
+
+    limparCampos(){
+        this.valor = 0;
+        this.destino = 0;
     }
 }
