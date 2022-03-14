509430e53baec1716c13d9da876483bd53998781
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2022-03-14 01:00:04 -0300

    Acerto na formatação da data e no layout da tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+
+registerLocaleData(localePt, 'pt')
 
 @NgModule({
   declarations: [
@@ -17,7 +21,13 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+    provide: DEFAULT_CURRENCY_CODE,
+    useValue: 'BRL',
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
