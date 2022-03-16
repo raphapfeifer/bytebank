9725128edaca58cbc24ac255883da60f242ac6f1
raphael.alcateia <raphael.alcateia@salcisne.com.br>
2022-03-16 11:33:15 -0300

    Criação da service

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Input } from '@angular/core';
+import { TransferenciaService } from '../services/transferencia';
 
 @Component({
   selector: 'app-extrato',
@@ -7,11 +8,12 @@ import { Component, OnInit,Input } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
-  @Input() transferencias: any[];
+  transferencias: any[];
 
-  constructor() { }
+  constructor(private service: TransferenciaService) { }
 
   ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
   }
 
 }
